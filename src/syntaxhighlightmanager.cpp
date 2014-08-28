93db94c6430a8f884b0dc4d8dd88768cf8f686ca
Leonardo Banderali <lbanderali@yahoo.com>
2014-08-27 22:35:15 -0600

    Implement basic functionality (no interface) for the language/lexer manager. On branch master, changes to be committed: 	modified:   src/syntaxhighlightmanager.cpp 	modified:   src/syntaxhighlightmanager.h

--- src/syntaxhighlightmanager.cpp
+++ src/syntaxhighlightmanager.cpp
@@ -3,7 +3,7 @@ Project: Lepton Editor
 File: syntaxhighlightmanager.cpp
 Author: Leonardo Banderali
 Created: August 26, 2014
-Last Modified: August 26, 2014
+Last Modified: August 27, 2014
 
 Description:
     Lepton Editor is a text editor oriented towards programmers.  It's intended to be a
@@ -39,12 +39,108 @@ Usage Agreement:
 //include this file's header
 #include "syntaxhighlightmanager.h"
 
+//include Qt classes
+#include <QString>
+#include <QDir>
+#include <QFileInfo>
+#include <QFileInfoList>
+#include <QDomDocument>
+#include <QDomElement>
+#include <QRegularExpression>
+
+//include QScintilla classes
+#include <Qsci/qscilexerhtml.h>
+#include <Qsci/qscilexercss.h>
+#include <Qsci/qscilexerxml.h>
+#include <Qsci/qscilexeryaml.h>
+
+//include Lepton files which are needed by this class
+#include "leptonlexer.h"
+
+//include Lepton files used for this class implementation
+#include "generalconfig.h"
 
 
-SyntaxHighlightManager::SyntaxHighlightManager() {
+
+SyntaxHighlightManager::SyntaxHighlightManager(ScintillaEditor* _parent) {
 /* -Class constructor */
+
+    //initialize private data members
+    parent = _parent;                   //initialize parent object
+
+    //get the language files' directory
+    QString languagesDirPath = GeneralConfig::getLangsDirPath();    //get path to the directory where the language files are
+    QDir languagesDir(languagesDirPath);                            //create a directory object for it
+
+    //define the normal language actions based on the data from language files
+    if ( languagesDir.exists() ) {      //only proceed if the languages directory actually exists (which it should)
+        QFileInfoList languageFiles = languagesDir.entryInfoList( QStringList("*.xml"), QDir::Files ); //get a list of all the XML files (language files)
+
+        foreach(const QFileInfo& languageFile, languageFiles) { //for every language file found
+            QFile langFileObject( languageFile.absoluteFilePath() );    //create an object to read the contents of the file
+
+            QDomDocument languageData("languageXmlData");               //create an XML DOM object to store the language data
+            if (! languageData.setContent(&langFileObject) ) continue;  //get the XML data from the language file
+            QDomElement languageElement = languageData.documentElement();//get root element of the XML document
+
+            /*##############################################################
+            ### I have chosen to use the following code instead of using  ##
+            ###  `if (languageElement.tagName() == "language") continue;` ##
+            ### because I may later decide later on to do something       ##
+            ### different when a  different root element is defined.      ##
+            ##############################################################*/
+            if (languageElement.tagName() == "language") {              //if the file defines a programming language
+                QString languageName = languageElement.attribute("name");   //get the name of the language that will be dipslayed in the language menu
+                if ( languageName.isEmpty() ) continue;                     //if no 'name' attribute is defined, consider the DOM data as invalid and continue
+
+                QAction* languageAction = new QAction(languageName, languageActions);//create a menu action for the language
+                languageAction->setCheckable(true);                         //set the action to be checkable when it is clickec/selected in the language menu
+                languageAction->setChecked(false);                          //explicitly set the action as being unchecked by default (note strictly required)
+                languageAction->setData( languageFile.absoluteFilePath() ); //set the data of the action as the path to the language file
+
+                if ( languageElement.hasAttribute("ext") ) {                //if a list of extensions is given, add it to the extension table for lookup
+                    QStringList extList = languageElement.attribute("ext").split( QRegularExpression("\\s") );  //split the extension list at white spaces
+                    ExtensionActionPair pair(extList, languageAction);      //associate the extensions with the language action
+                    fileExtensionTable.append(pair);                        //add the pair to the table
+                }
+
+                languageActions->addAction(languageAction);                 //add language action to the action group
+            }
+        }
+
+        //define list of specialized lexers
+        languageActions->addAction(new QAction(languageActions))->setSeparator(true); //%% may need to be removed (not sure what this does) %%
+        QAction* langAction = languageActions->addAction("Plain Text");
+        langAction->setCheckable(true);
+        langAction->setChecked(true);
+        specialLanguages[langAction] = new LeptonLexer();
+
+        langAction = languageActions->addAction("HTML");
+        langAction->setCheckable(true);
+        langAction->setChecked(false);
+        specialLanguages[langAction] = new QsciLexerHTML();
+
+        langAction = languageActions->addAction("CSS");
+        langAction->setCheckable(true);
+        langAction->setChecked(false);
+        specialLanguages[langAction] = new QsciLexerCSS();
+
+        langAction = languageActions->addAction("XML");
+        langAction->setCheckable(true);
+        langAction->setChecked(false);
+        specialLanguages[langAction] = new QsciLexerXML();
+
+        langAction = languageActions->addAction("YAML");
+        langAction->setCheckable(true);
+        langAction->setChecked(false);
+        specialLanguages[langAction] = new QsciLexerYAML();
+
+        //add all language actions to the language menu
+        languageMenu->addActions(languageActions->actions());
+    }
 }
 
 SyntaxHighlightManager::~SyntaxHighlightManager() {
 /* -Class destructor */
+    parent = 0;
 }
--- src/syntaxhighlightmanager.h
+++ src/syntaxhighlightmanager.h
@@ -3,7 +3,7 @@ Project: Lepton Editor
 File: syntaxhighlightmanager.h
 Author: Leonardo Banderali
 Created: August 26, 2014
-Last Modified: August 26, 2014
+Last Modified: August 27, 2014
 
 Description:
     Lepton Editor is a text editor oriented towards programmers.  It's intended to be a
@@ -39,15 +39,75 @@ Usage Agreement:
 #ifndef SYNTAXHIGHLIGHTMANAGER_H
 #define SYNTAXHIGHLIGHTMANAGER_H
 
+//include Qt classes
+#include <QAction>
+#include <QActionGroup>
+#include <QMenu>
+#include <QList>
+#include <QStringList>
+#include <QHash>
+
+//include QScintilla classes
+#include <Qsci/qscilexer.h>
+
+//include Lepton files which are needed by this class
+#include "scintillaeditor.h"
+
+
+
 class SyntaxHighlightManager {
 /* -A class that provides an interface for managing the language lexers used in the ScintillaEditor class. */
 
     public:
-        SyntaxHighlightManager();
-        /* -Class constructor */
+        explicit SyntaxHighlightManager(ScintillaEditor* _parent);
+        /*
+            -Class constructor
+            -initialize the parent object which points to the ScintillaEditor class which uses this manager.
+        */
 
-        ~SyntaxHighlightManager();
-        /* -Class destructor */
+        virtual ~SyntaxHighlightManager();
+        /*
+            -Class destructor
+            -perform garbage collections (free allocated memory and null all pointers)
+        */
+
+        class ExtensionActionPair {
+        /* -A class that stores a list of file extensions and associates it to a language action */
+            public:
+                explicit ExtensionActionPair(QStringList _extensionList, QAction* _action) {
+                /* -the class constructor initializes the data members */
+                    extensionList = _extensionList;
+                    action = _action;
+                }
+
+                bool hasExtension(const QString& extension) const {
+                /* -return true if the extension is in the list, false otherwise */
+                    if ( extensionList.contains(extension) ) return true;
+                    else return false;
+                }
+
+                QAction* getLanguageAction() const {
+                /* -return the language action */
+                    return action;
+                }
+
+                QAction* getActionIfHasExt(const QString& extension) const {
+                /* -return the language action if the extension passed is in the list, else return a 0 pointer */
+                    if ( extensionList.contains(extension) ) return action;
+                    else return 0;
+                }
+
+            private:
+                QStringList extensionList;
+                QAction* action;
+        };
+
+    private:
+        ScintillaEditor* parent;                        //pointer the editing class which uses this manager
+        QActionGroup* languageActions;                  //a group of actions to use for selecting a syntax highlighting language
+        QMenu* languageMenu;                            //a menu to select a language
+        QList<ExtensionActionPair> fileExtensionTable;  //a table to match file extensions with an action to select a language
+        QHash<QAction*, QsciLexer*> specialLanguages;   //a list of specialized languages/lexers which do not require user definition
 };
 
 #endif // SYNTAXHIGHLIGHTMANAGER_H
